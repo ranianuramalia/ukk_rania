653b1e838f3d538354946e70bddbc690b30287db
Mila-Nirmala <mila.nirmala2007@smkn4-tsm.sch.id>
2025-10-06 11:16:49 +0700

    senin

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="col-lg-12">
             <div class="row">
                 <div class="col-lg-3">
-                    <div class="container mt-5 text-light">
+                    <div class="container mt-5 text-light text-center">
                         <h4>SMKN 4 Tasikmalaya</h4>
                         <hr>
                         <p> Jl. Depok, Sukamenak, Kec. Purbaratu, Kab. Tasikmalaya, Jawa Barat 46196</p>
@@ -12,7 +12,7 @@
                 </div>
                 <div class="col-lg-3">
                     <div class="container mt-5 text-light text-center">
-                        <h5>Alamat</h5>
+                        <h4>Alamat</h4>
                         <hr>
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.213116362029!2d108.25189907500041!3d-7.329945292678408!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f59eeb6bda36f%3A0xa9e724a275da6c2d!2sSMK%20Negeri%204%20Tasikmalaya!5e0!3m2!1sid!2sid!4v1755672850218!5m2!1sid!2sid"
@@ -21,19 +21,19 @@
                     </div>
                 </div>
                 <div class="col-lg-3">
-                    <div class="container mt-5 text-light">
+                    <div class="container mt-5 text-light text-center">
                         <h4>Media Sosial</h4>
                         <hr>
-                        <a href="https://www.youtube.com/@smkn4tasikmalayamediaoffic704" class="text-white text-decoration-none">Youtube</a><br/>
-                        <a href="https://www.instagram.com/smkn4tsmofficial/" class="text-white text-decoration-none">Instagram</a><br/>
+                        <a href="https://www.youtube.com/@smkn4tasikmalayamediaoffic704" class="text-white text-decoration-none">Youtube</a><br />
+                        <a href="https://www.instagram.com/smkn4tsmofficial/" class="text-white text-decoration-none">Instagram</a><br />
                         <a href="https://www.facebook.com/4smkn/" class="text-white text-decoration-none">Facebook</a>
                     </div>
                 </div>
                 <div class="col-lg-3">
-                    <div class="container mt-5 text-light">
+                    <div class="container mt-5 text-light text-center">
                         <h4>Link External</h4>
                         <hr>
-                        <a href="https://kemendikdasmen.go.id/" class="text-white text-decoration-none">Kemendikbud</a><br/>
+                        <a href="https://kemendikdasmen.go.id/" class="text-white text-decoration-none">Kemendikbud</a><br />
                         <a href="https://dapo.kemendikdasmen.go.id/sekolah/88C1EAB7D23D6A8BFF61" class="text-white text-decoration-none">Dapodik</a><br />
                         <a href="https://psmk.jabarprov.go.id/" class="text-white text-decoration-none">PSMK</a><br />
                         <a href="https://disdik.jabarprov.go.id/" class="text-white text-decoration-none">Disdik Jabar</a>
@@ -52,4 +52,3 @@
             <hr>
         </div>
     </div>
-    
